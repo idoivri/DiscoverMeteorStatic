83aee6613e6b69adeb30b2448d8c44bdb4777088
Sacha Greif <sacha357@gmail.com>
2014-10-03 11:42:07 +0900

    adding hebrew

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ DEFAULTLANG = 'ru'
 # Blog settings
 ###
 
-LANGUAGES = ['de', 'en', 'es', 'fr', 'gr', 'id', 'it', 'jp', 'kr', 'nl', 'pl', 'pt', 'ru', 'zh', 'ro', 'ar', 'da', 'hr', 'ms', 'tr', 'am', 'th', 'uk', 'hi', 'sv', 'hu']
+LANGUAGES = ['de', 'en', 'es', 'fr', 'gr', 'id', 'it', 'jp', 'kr', 'nl', 'pl', 'pt', 'ru', 'zh', 'ro', 'ar', 'da', 'hr', 'ms', 'tr', 'am', 'th', 'uk', 'hi', 'sv', 'hu', 'he']
 
 LANG = ENV['DMLANG'] || DEFAULTLANG
 

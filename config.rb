5e236da5a41c4adc0531bc110c2f5594176ed881
Sacha Greif <sacha357@gmail.com>
2014-11-06 10:32:07 +0900

    adding Bangla and moving jp/ja

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ DEFAULTLANG = 'es'
 # Blog settings
 ###
 
-LANGUAGES = ['de', 'en', 'es', 'fr', 'gr', 'id', 'it', 'ja', 'kr', 'nl', 'pl', 'pt', 'ru', 'zh', 'ro', 'ar', 'da', 'hr', 'ms', 'tr', 'am', 'th', 'uk', 'hi', 'sv', 'hu', 'he']
+LANGUAGES = ['de', 'en', 'es', 'fr', 'gr', 'id', 'it', 'ja', 'kr', 'nl', 'pl', 'pt', 'ru', 'zh', 'ro', 'ar', 'da', 'hr', 'ms', 'tr', 'am', 'th', 'uk', 'hi', 'sv', 'hu', 'he', 'bn']
 
 LANG = ENV['DMLANG'] || DEFAULTLANG
 
